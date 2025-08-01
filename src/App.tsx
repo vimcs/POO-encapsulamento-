746dd7a2f7bf72fc98251d1940eac65482704bf4
norevendeLee <norevende.lee@gmail>
2025-08-01 10:32:52 -0300

    Usuário pode fazer o deposito e o saque na conta

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
 import './App.css';
 import { ContaBancaria } from './Models/ContaBancaria';
+import { useState } from 'react';
 
 function App() {
-  const conta = new ContaBancaria();
-  conta.depositar(100);
-  conta.sacar(40);
+  const [conta] = useState(new ContaBancaria());
+  const [saldo, setSaldo] = useState(conta.verSaldo());
+  const [valor, setValor] = useState(0);
 
-  return <p>Saldo disponível: {conta.verSaldo()}</p>;
+  const handleDeposito = () => {
+    conta.depositar(valor);
+    setSaldo(conta.verSaldo());
+  };
+
+  const handleSaque = () => {
+    conta.sacar(valor);
+    setSaldo(conta.verSaldo());
+  };
+
+  return (
+    <div>
+      <h2>Banco Simples</h2>
+      <p>Saldo: R$ {saldo}</p>
+
+      <input
+        type="number"
+        value={valor}
+        onChange={(e) => setValor(Number(e.target.value))}
+      />
+      <br />
+      <button onClick={handleDeposito}>Depositar</button>
+      <button onClick={handleSaque}>Sacar</button>
+    </div>
+  );
 }
 
 export default App;
